2290a450345ed6a4273a214899ff96b4bd019f7b
ad-iel <mahs0003@student.monash.edu>
2025-04-07 13:52:08 +0800

    Only Task 1 and 2 are done, Task 3 and 4 are untouched

--- Task_1.c
+++ Task_1.c
@@ -26,26 +26,30 @@ argument, only the filtered string must be printed onto the terminal.
 
 void filter_ascending(char* string_1);
 
-int main()
+int main(int argc, char* argv[])
 {
-    char string_1[20];
-    printf("Enter the string: ");
-    scanf("%s", string_1);
-    filter_ascending(string_1);
+    //char string_1[20];
+    //printf("Enter the string: ");
+    //scanf("%s", string_1);
+    if (argc > 1)
+    {
+        // do nothing
+    }
+    filter_ascending(argv[1]);
     printf("The modified string is: ");
-    printf("%s\n", string_1);
+    printf("%s\n", argv[1]);
 	return 0;
 }
 
 void filter_ascending(char* string_1)
 {
-    for (int i = 1; i < strlen(string_1); i++)      // HELLO PEOPLE!!!!!
+    for (int i = 1; i < strlen(string_1); i++)      
     {
-        for (int j = 0; string_1[j+2] != '\0'; j++)
+        for (int j = 0; string_1[j+1] != '\0'; j++)     // Corrected j+2 -> j+1
         {
             if (string_1[j] > string_1[j+1])
             {
-                for (int k = 0; string_1[k] != '\0'; k++)
+                for (int k = j+1; string_1[k] != '\0'; k++)     // Corrected j+2 -> j+1
                 {
                     string_1[k] = string_1[k+1];
                 }
